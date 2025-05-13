5d846b960d7e098138b5405e556d12c991b3bebd
Diego Barrado <Diego.barrado@gmail.com>
2025-05-13 16:03:23 -0300

    testpost

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from "./index";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+});
+it("Deve permitir comentario corretamente", () => {
+  render(<PostComment />);
+  expect(screen.getByText("Comentar")).toBeInTheDocument();
+  const inputComentario = screen.getByTestId("campo-comentario");
+  const botaoComentar = screen.getByTestId("botão-comentario");
+  const areaComantarios = screen.getByTestId("lista-comentarios");
+  fireEvent.change(inputComentario, {
+    target: { value: "Teste Primeiro Comentario" },
+  });
+  fireEvent.click(botaoComentar);
+  expect(screen.getByText("Teste Primeiro Comentario")).toBeInTheDocument();
+  fireEvent.change(inputComentario, {
+    target: { value: "Teste Comentario 2" },
+  });
+  fireEvent.click(botaoComentar);
+  expect(screen.getByText("Teste Comentario 2")).toBeInTheDocument();
+  expect(areaComantarios.children.length).toBe(2);
 });
